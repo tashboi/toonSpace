b950ca7b2647ab99454c709db4be9fd319ca8f84
tashboi <w18011022@northumbria.ac.uk>
2022-05-12 21:00:42 +0100

    Mahdi's updated files

--- Frontend/src/components/Mahdi/Eventsfeed.js
+++ Frontend/src/components/Mahdi/Eventsfeed.js
@@ -1,62 +1,55 @@
 import React from "react";
 import Eventcomp from "./Eventcomp.js";
-import './mahdi.css';
-class Eventsfeed extends React.Component{
+import "./mahdi.css";
+class Eventsfeed extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { results: [] };
+  }
+  componentDidMount() {
+    const url =
+      "http://unn-w18017488.newnumyspace.co.uk/year3/assignment/api/events";
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({ results: data.results });
+      })
+      .catch((err) => {
+        console.log("something went wrong ", err);
+      });
+  }
 
-    constructor(props){
-        super(props)
-        this.state = {results : []}
-    
-    }
-componentDidMount(){
-    const url ="http://unn-w18017488.newnumyspace.co.uk/year3/assignment/api/events"
-     fetch(url)
-    .then( (response) => { 
-        return response.json() 
-    })
-    .then( (data) => {
-        this.setState({results: data.results})
-    })
-    .catch ((err) => { 
-        console.log("something went wrong ", err) 
-    });
-}
+  render() {
+    console.log(this.state.results);
+    return (
+      <div class="body">
+          
+        <div className="eventswindow">
+        <h1>EventsFeed</h1>
+          {this.state.results.map((event, i) => (
+            <Eventcomp key={i} event={event} />
+          ))}
+        </div>
+        <div class="pagetitle">
+          <h1>EventsFeed page</h1>
+        </div>
 
-render (){
-   
-    console.log(this.state.results)
-    return<div>
-
-            <div className="eventswindow">
-               <h1>events Feed</h1>
-                {this.state.results.map( (event, i) => (<Eventcomp key={i} event={event}/>) )}
-                </div>
-                <div>
-                <table>
-  <tr>
-    <th>Company</th>
-    <th>Contact</th>
-    <th>Country</th>
-  </tr>
-  <tr>
-    <td>Alfreds Futterkiste</td>
-    <td>Maria Anders</td>
-    <td>Germany</td>
-  </tr>
-  <tr>
-    <td>Centro comercial Moctezuma</td>
-    <td>Francisco Chang</td>
-    <td>Mexico</td>
-  </tr>
-</table>
-</div>
-
-
-                </div>
-           
-  
-    
-}
-
+        <div class="itinarary-table">
+            <h2>Itinarary</h2>
+          <table>
+            <tr>
+              <th>event Name</th>
+              <th>description</th>
+              <th>event location</th>
+              <th>event type</th>
+            </tr>
+            
+          </table>
+        </div>
+      </div>
+    );
+  }
 }
 export default Eventsfeed;
